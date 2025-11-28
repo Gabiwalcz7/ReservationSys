b223b99a3687d4a1df94a149a095eaf6d6b0a9bc
Gabi <g.walczowska@gmail.com>
2025-11-28 18:33:38 +0100

    add ResourceController

---
+++ ReservationSystem/Controllers/ResourcesController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ReservationSystem.Data;
+using ReservationSystem.Entities;
+
+namespace ReservationSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResourcesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ResourcesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET ALL
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Resource>>> GetResources()
+        {
+            return await _context.Resources
+                .Include(r => r.ResourceType)
+                .ToListAsync();
+        }
+
+        //Get by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Resource>> GetResource(int id)
+        {
+            var resource = await _context.Resources
+                .Include(r => r.ResourceType)
+                .FirstOrDefaultAsync(r => r.Id == id);
+
+            if (resource == null)
+                return NotFound();
+
+            return resource;
+        }
+
+        //CREATE
+        [HttpPost]
+        public async Task<ActionResult<Resource>> CreateResource(Resource resource)
+        {
+            _context.Resources.Add(resource);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetResource), new { id = resource.Id }, resource);
+        }
+
+        //UPDATE
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateResource(int id, Resource resource)
+        {
+            if (id != resource.Id)
+                return BadRequest();
+
+            _context.Entry(resource).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        //DELETE
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteResource(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+                return NotFound();
+
+            _context.Resources.Remove(resource);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
